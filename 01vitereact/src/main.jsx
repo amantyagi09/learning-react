399c2435e5a8eaa5b6869666b61f6ffe52b25e65
Aman Kumar <tyagi13aman@gmail.com>
2026-01-19 23:12:15 +0530

    learned how to make custum react code

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// const ReactElement = React.createElement(
+//   'a',
+//   {href: 'https://google.com',
+//     target: '_blank',
+//   },
+//   'click me to visit google'
+// )
+// this is how an object is passed to the react(in the form of function that is returnng the elements)
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
